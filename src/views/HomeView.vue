f1b0f5bb69dc756f0a6d14cf83f686b559332dca
chow5566 <767656961@qq.com>
2023-12-15 16:38:45 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
   <div class="flex">
     <el-button type="primary">test</el-button>
     <el-button type="primary" plain>test</el-button>
-    1
+    2
   </div>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
